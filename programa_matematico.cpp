d6fdfec3eb9b74580d9691feacd0e701621b4852
Leonardo Hdez <perros_rebeldes@hotmail.com>
2019-10-24 14:25:52 -0500

    Correccion del algoritmo promedio.

--- programa_matematico.cpp
+++ programa_matematico.cpp
@@ -1,16 +1,19 @@
-int n;
+int promedio(){
+    int n;
     float a,c=0,promedio;
     
-    cout<<"Ingrese la cantidad de numeros: ";
+    cout<<"Ingrese la cantidad de calificaciones a evaluar: ";
     cin>>n;
     for(int i=0;i<n;i++)
     {
-        cout<<"Ingrese el numero "<<i+1<<":";
+        cout<<"Ingrese la calificacion "<<i+1<<":";
         cin>>a;
         c=c+a;
     }
     
     promedio=c/n;
-    cout<<"el promedio es:"<<promedio;
+    cout<<"El promedio es:"<<promedio;
 
     return=0;
+}
+
